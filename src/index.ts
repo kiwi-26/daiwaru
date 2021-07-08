da3b1dd591540e7da9ec7fa35058dd795b8cdd6b
kiwi-26 <winterwind.sh0206@gmail.com>
2021-07-09 00:29:15 +0900

    テンプレートHTMLを読み込んでPDF出力

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
+import fs from 'fs/promises';
+import puppeteer from 'puppeteer';
 
-console.log(hello("World"));
+(async() => {
+  const template = (await fs.readFile('templates/index.html')).toString();
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // PDF出力対象ページ
+  await page.setContent(template);
+
+  // PDF作成処理
+  await page.pdf({
+    path: 'result.pdf',
+  });
+
+  browser.close();
+  console.log('PDF出力完了');
+})();
