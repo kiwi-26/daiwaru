ff2cabd474bd2ff0702ac9a927262b4bf19f039c
kiwi-26 <winterwind.sh0206@gmail.com>
2021-07-09 01:04:52 +0900

    ページ割りの内容をスクリプトから渡すようにした

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,43 @@ import fs from 'fs/promises';
 import ejs from 'ejs';
 import puppeteer from 'puppeteer';
 
+const spreads = [
+  {
+    pages: [
+      {type: 'clear'},
+      {title: '表紙', folio: '表1'},
+    ]
+  },
+  {
+    pages: [
+      {title: '表紙', folio: '表2'},
+      {title: 'コンテンツ', folio: '1'},
+    ]
+  },
+  {
+    pages: [
+      {title: 'コンテンツ', folio: '2'},
+      {title: 'コンテンツ', folio: '3'},
+    ]
+  },
+  {
+    pages: [
+      {title: 'コンテンツ', folio: '4'},
+      {title: '裏表紙', folio: '表3'},
+    ]
+  },
+  {
+    pages: [
+      {title: '裏表紙', folio: '表4'},
+      {type: 'clear'},
+    ]
+  },
+];
+
 (async() => {
   const template = await ejs.renderFile('templates/index.ejs', {
-    document_title: 'My page plot'
+    document_title: 'My page plot',
+    spreads: spreads
   }, {
     async: true
   })
